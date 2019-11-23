c929d81a614ceccf831c8560c8de85143e131dc6
Ricardo Alves <ricardospalves@gmail.com>
2019-11-22 23:34:57 -0300

    Adicionando a classe de matrizes

---
+++ matrix.js
@@ -0,0 +1,63 @@
+'use strict'
+
+class Matrix {
+  constructor({ rows, cols }) {
+    this.rows = rows
+    this.cols = cols
+    this.data = []
+
+    for(let i = 0; i < this.rows; i++) {
+      const array = []
+
+      for(let j = 0; j < this.cols; j++) {
+        const randomNumber = Math.floor(Math.random() * 10)
+
+        array.push(randomNumber)
+      }
+
+      this.data.push(array)
+    }
+  }
+
+  map(func) {
+    this.data = this.data.map((row, rowIndex) => {
+      return row.map((col, colIndex) => {
+        return func(col, rowIndex, colIndex)
+      })
+    })
+
+    return this
+  }
+
+  static sum(a, b) {
+    const matrix = new Matrix(a)
+
+    matrix.map((col, rowIndex, colIndex) => {
+      return a.data[rowIndex][colIndex] + b.data[rowIndex][colIndex]
+    })
+
+    return matrix
+  }
+
+  static multiply(a, b) {
+    const matrix = new Matrix({
+      rows: a.rows,
+      cols: b.cols
+    })
+
+    matrix.map((col, colIndex, rowIndex) => {
+      let sum = 0
+
+      for(let i = 0; i < b.rows; i++) {
+        const element1 = a.data[colIndex][i]
+        const element2 = b.data[i][rowIndex]
+
+        sum += element1 * element2
+      }
+
+      return sum
+    })
+
+    return matrix
+  }
+}
